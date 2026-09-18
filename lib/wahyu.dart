20fdfbd8827f05f5b9f402405eafac2db795da23
Wahyu Nugroho <triwahyunugroho78@gmail.com>
2026-09-18 16:29:03 +0700

    halaman baru

--- lib/wahyu.dart
+++ lib/wahyu.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Wahyu());
-}
-
 class Wahyu extends StatelessWidget {
   const new({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Profil Mahasiswa',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.lightBlue)),
+      home: const Beranda(title: 'Profil Tri Wahyu Nugroho'),
+    );
+  }
+}
+
+class Beranda extends StatefulWidget {
+  const new({super.key, required this.title});
+  final String title;
+  @override
+  State<Beranda> createState() => _BerandaState();
+}
+
+class _BerandaState extends State<Beranda> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              padding: EdgeInsets.only(bottom: 20),
+              child: Image.asset('images/FOTO KTM.png'),
+            ),
+            Text(style: TextStyle(fontSize: 20), 'TRI WAHYU NUGROHO'),
+            Text(style: TextStyle(fontSize: 20), '701240078'),
+            Text(style: TextStyle(fontSize: 20), 'SISTEM INFORMASI'),
+            Text(
+              style: TextStyle(fontSize: 20),
+              'UIN SULTAN THAHA SAIFUDDIN JAMBI',
+            ),
+            Text(style: TextStyle(fontSize: 20), 'PEMROGRAMAN MOBILE'),
+          ],
+        ),
+      ),
+    );
   }
 }
