49a8a98b2330641cbecd39d87d3de18166072dcb
Wahyu Nugroho <triwahyunugroho78@gmail.com>
2026-09-18 14:01:17 +0700

    first commit

---
+++ lib/wahyu.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Wahyu());
+}
+
+class Wahyu extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
